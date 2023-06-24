c4cc01c5f4f2eaa8c4d28ca4242f7d565aec6158
Natasha <natashachahwda370@gmail.com>
2023-06-24 16:54:13 +0530

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function Card() {
+    return (
+        <div>
+            <div>
+                <div className="card mt-3" style={{ "width": "18rem","maxHeight":"360px" }}>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK6RUdKeSXk0X7Fj7U1gx06qN1U9tGOiUPyQ&usqp=CAU" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This is some important text</p>
+                        <div className='container w-100' >
+                            <select className='m-2 h-100 bg-success rounded'>
+                                {Array.from(Array(6), (e, i) => {
+                                    return (
+                                        <option value={i + 1}> {i + 1}</option>
+                                    )
+                                })
+                                }
+                            </select>
+                            <select className='m-2 h-100 bg-success rounded'>
+                                <option value="half">Half</option>
+                                <option value="full">Null</option>
+                            </select>
+                            <div className='d-inline h-100 fs-5'>
+                                Total price
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
---
+++ src/screens/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Card from '../components/Card'
+import Carousal from '../components/Carousal'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+export default function Home() {
+  return (
+    <div>
+    <div><Navbar/></div>
+    <div><Carousal/></div>
+    <div className='m-3'>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      </div>
+    <div><Footer/></div>
+     </div>
+  )
+}
